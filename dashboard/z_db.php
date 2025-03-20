31c4206650e70a3b6104f10c314d72848f4e5f1d
Sajahan Mansoor <sjeel663@gmail.com>
2025-03-20 13:54:13 +0300

    added all files related fggroup

---
+++ dashboard/z_db.php
@@ -0,0 +1,19 @@
+<?php
+$con = new mysqli("localhost", "u833413474_fgroot", "Fggroup786786786", "u833413474_fggroup");
+if ($con->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+
+//Your Website URL Goes Here
+$url="http://localhost/public_html/dashboard";
+
+
+//Set Blog Activation Bonus Here (It must be only Number)
+$blog_bonus ="10";
+//Set Article Activation Bonus Here (It must be only Number)
+$art_bonus="10";
+//Set Daily Login Bonus Here (It must be only Number)
+$login_bonus="10";
+//Set Currency Symbol for daily login bonus Here
+$money="$";
+?>
